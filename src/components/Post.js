4f50e71a7e93cbb83b4404c73a42d99fd6b8d9b8
gustavfaustino <gustav.faustino107@gmail.com>
2024-10-26 02:03:46 -0300

    Pra curtir algo, so pra testar

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import './Post.css';
 import { PiArrowBendUpLeftBold } from "react-icons/pi";
 import { BiRepost } from "react-icons/bi";
@@ -7,6 +7,20 @@ import { IoMdStarOutline } from "react-icons/io";
 import { IoIosMore } from "react-icons/io";
 
 export default function Post(props) {
+    // Estados para curtidas e controle de curtida
+    const [likes, setLikes] = useState(0);
+    const [liked, setLiked] = useState(false);
+
+    // Função para alternar curtida
+    const handleLike = () => {
+        if (liked) {
+            setLikes(likes - 1); // Remove curtida
+        } else {
+            setLikes(likes + 1); // Adiciona curtida
+        }
+        setLiked(!liked); // Alterna o estado de curtida
+    };
+
     return (
         <div className="Post">
             <div className="PostHeader">
@@ -30,7 +44,13 @@ export default function Post(props) {
                 <button className="OptionBtn"><PiArrowBendUpLeftBold /></button>
                 <button className="OptionBtn"><BiRepost /></button>
                 <button className="OptionBtn"><FaRegBookmark /></button>
-                <button className="OptionBtn"><IoMdStarOutline /></button>
+                
+                {/* Botão de Curtida com contador */}
+                <button className="OptionBtn" onClick={handleLike}>
+                    <IoMdStarOutline />
+                    <span className="LikeCount">{likes}</span>
+                </button>
+
                 <button className="OptionBtn"><IoIosMore /></button>
             </div>
         </div>
